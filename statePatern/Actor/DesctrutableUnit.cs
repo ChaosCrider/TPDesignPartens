43ca67d406950596d9bdbf271c85d38d34e5f468
ChaosCrider <ChaosCrider@hotmail.com>
2024-02-26 13:13:22 -0500

    Skeleton-Abstract
    now, the Abstract class

---
+++ statePatern/Actor/DesctrutableUnit.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using TPDesignPartens.statePatern.Actor;
+using TPDesignPartens.statePatern.Scene;
+
+namespace TPDesignPartens.statePatern.State
+{
+    public class DesctrutableUnit
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public bool isMobile { get; set; }
+        public bool isfortified { get; set; }
+        public int HP { get; set; }
+        public int tange { get; set; }
+        public int lineOfSight { get; set; }
+        public int moveSpeed { get; set; }
+        public Stance stance { get; set; }
+        public List<IAbility> abilities { get; set; }
+        public Location location { get; set; }
+        public Stance availableStance { get; set; }
+
+        private delegate void runAbility(IAbility ability);
+
+        public DesctrutableUnit(List<IAbility> abilities, Stance availableStance)
+        {
+            this.abilities = abilities;
+            this.availableStance = availableStance;
+            runAbility triggeringAbility = new runAbility(doAbility);
+
+        }
+
+        private void doAbility(IAbility ability)
+        {
+            Console.WriteLine("Null Ability triggered");
+        }
+
+        public List<IAbility> listAbilities() 
+        {
+            return new List<IAbility>();
+        }
+
+        public void useAbility(IAbility ability) 
+        {
+            doAbility(ability);
+        }
+
+    }
+}
---
+++ statePatern/State/Stance.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPDesignPartens.statePatern.Scene;
+
+namespace TPDesignPartens.statePatern.State
+{
+    public class Stance : IStance
+    {
+        public string name { get; set; }
+        public DesctrutableUnit owner  { get; set; }
+
+        //needed for units that can attack
+        delegate void runAttack(ITargetable target);
+        runAttack doAttack = null;
+
+        //needed to update the units implicite statistique and potentical explicit statistique.
+        delegate void runUpdate(DesctrutableUnit unit);
+        runUpdate doUpdate = null;
+
+        public Stance(string name, DesctrutableUnit owner)
+        {
+            this.name = name;
+            this.owner = owner;
+            doAttack = new runAttack(noAttack);
+            doUpdate = new runUpdate(noUpdate);
+        }
+
+        private void noUpdate(DesctrutableUnit unit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void noAttack(ITargetable target) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public void attack(ITargetable target)
+        {
+            doAttack(target);
+        }
+
+        public void changeState(IStance stance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void move(ITargetable target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void stop(ITargetable owner)
+        {
+            
+        }
+
+        public void updateAttribut(DesctrutableUnit owner)
+        {
+            doUpdate(owner);
+        }
+    }
+}
